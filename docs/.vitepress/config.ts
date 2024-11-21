bfae0e81427eb2201b4cf80d95148e2beba98a5c
durunsong <durunsong@sailvan.com>
2024-11-21 16:29:54 +0800

    🍋feat：添加配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vitepress';
+import { defineConfig } from "vitepress";
 
 export default defineConfig({
-  title: 'My Blog',
-  description: 'This is my VitePress blog.',
+  title: "My Blog",
+  description: "This is my VitePress blog.",
   themeConfig: {
-    algolia: {
-      appId: 'YOUR_APP_ID', // 替换为你的 Algolia 应用 ID
-      apiKey: 'YOUR_SEARCH_API_KEY', // 替换为你的搜索 API 密钥
-      indexName: 'YOUR_INDEX_NAME', // 替换为你的索引名称
+    // algolia: {
+    //   appId: "YOUR_APP_ID", // 替换为你的 Algolia 应用 ID
+    //   apiKey: "YOUR_SEARCH_API_KEY", // 替换为你的搜索 API 密钥
+    //   indexName: "YOUR_INDEX_NAME", // 替换为你的索引名称
+    // },
+    socialLinks: [
+        { icon: 'github', link: 'https://github.com/durunsong/my-blog' }
+      ],
+    search: {
+      provider: "local",
     },
   },
 });
